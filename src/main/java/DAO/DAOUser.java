e556e16ffd183b745c5fb23791f8842ac61b5377
Rocco Scavone <scavonerocco@hotmail.it>
2020-06-30 17:49:31 +0200

    Fixed Problem in DAOUser

--- src/main/java/DAO/DAOUser.java
+++ src/main/java/DAO/DAOUser.java
@@ -15,56 +15,44 @@ import org.slf4j.LoggerFactory;
 import model.User;
 
 public class DAOUser {
-	
+
 	private Logger LOG = LoggerFactory.getLogger(DAOUser.class);
-	
+
 	public void saveUser(User user) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the student object
-            session.save(user);
-            // commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-    }
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(user);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
 
 	public User getUserByEmail(String email) {
 		User user = new User();
-		try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-			user = (User) session.createQuery("From User U where U.email = :email").setParameter("email", email).uniqueResult();
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			user = (User) session.createQuery("From User U where U.email = :email").setParameter("email", email)
+					.uniqueResult();
 		}
 		return user;
 	}
-	
-    public boolean validate(String email, String password) {
-        Transaction transaction = null;
-        User user = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // get an user object
-            user = (User) session.createQuery("FROM User U WHERE U.email = :email").setParameter("email", email)
-                .uniqueResult();
-            this.LOG.info(user.toString());
-            if (user != null && user.getPassword().equals(password)) {
-                return true;
-            }
-            // commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        return false;
-    }
 
+	public boolean validate(String email, String password) {
+		User user = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			user = (User) session.createQuery("FROM User U WHERE U.email = :email").setParameter("email", email)
+					.uniqueResult();
+			if (user != null && user.getPassword().equals(password)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
